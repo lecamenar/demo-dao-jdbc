dd1c7f2c05f23a112cbb40fc435ab5b7fd3e2e34
Leticia Camenar <lcamenar@alunos.utfpr.edu.br>
2020-11-07 18:25:49 -0300

    reusing instatiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,6 +11,8 @@ public class Program {
 		
 		SellerDAO sellerDao = DaoFactory.createSellerDao();
 		
+		System.out.println("==== Teste 1: seller findById ====" );
+	
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 
