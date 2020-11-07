25d9d2a4a5e69b83efd26f51dcc183ce7fced94a
Leticia Camenar <lcamenar@alunos.utfpr.edu.br>
2020-11-07 20:35:18 -0300

    create DepartmentDAO implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,12 +1,17 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDAOJDBC;
 import model.dao.impl.SellerDAOJDBC;
 
 public class DaoFactory {
 	
 	public static SellerDAO createSellerDao() {
 		return new SellerDAOJDBC(DB.getConnection());
+	}
+	
+	public static DepartmentDAO createDepartmentDao() {
+		return new DepartmentDAOJDBC(DB.getConnection());
 	}
 
 }
